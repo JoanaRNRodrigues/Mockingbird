d1e919a77ce948c7b54e882d1b2c1bf64f720c78
Joana <95416213+JoanaRNRodrigues@users.noreply.github.com>
2022-06-12 21:23:43 +0100

    Add files via upload

---
+++ Data/MockingBirdDbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using MockingBird.Models;
+
+namespace MockingBird.Data
+{
+    public static class MockingBirdDbInitializer
+    {//function to insert data into the context
+        public static void InsertData(MockingBirdContext context)
+        {
+            // Adds a user
+            var user = new User
+            {
+                Name = "Mocking Bird",
+                UserName = "Mockingbird123",
+                Password = "password",
+                ConfirmPassword = "password",
+                Email = "mockingbird123@gmail.com",
+                ImagePath = "Admin_Image.jpg"
+            };
+
+            context.Users.Add(user);
+
+            // Adds a chirp
+            context.Chirps.Add(new Chirp
+            {
+                Id = 1,
+                Text = "Hello world",
+                User = user
+            });
+
+
+            // Saves changes
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Data/MockingBirdExtention.cs
@@ -0,0 +1,23 @@
+﻿using MockingBird.Models;
+
+namespace MockingBird.Data
+{
+    public static class MockingBirdExtention
+    {//if the database doesn't exist, then it will be created and data will be inserted with the MockingbirdDbInitializer
+        public static void CreateDbIfNotExists(this IHost host)
+        {
+            {
+                using (var scope = host.Services.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    var context = services.GetRequiredService<MockingBirdContext>();
+                    // Creates the database if not exists
+                    if (context.Database.EnsureCreated())
+                    {
+                        MockingBirdDbInitializer.InsertData(context);
+                    }
+                }
+            }
+        }
+    }
+}
